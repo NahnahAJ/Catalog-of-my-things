9837a1354bace0ba3ca57e16d6659c68ff6ed4a8
Aminu Rabiu <arabiu033@gmail.com>
2023-01-10 12:22:30 +0100

    Fix linters

--- main.rb
+++ main.rb
@@ -30,7 +30,7 @@ class Main
   def add_music_album
     inputs = music_input
     album = MusicAlbum.new(inputs[0].strip.capitalize,
-                      inputs[3].strip[0].capitalize == 'Y', inputs[2])
+                           inputs[3].strip[0].capitalize == 'Y', inputs[2])
     @albums << album
     genre = nil
     @genres.each do |g|
