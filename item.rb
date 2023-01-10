6465aa7983901e66a0950563fb53f3920955f4ef
Aminu Rabiu <arabiu033@gmail.com>
2023-01-10 12:10:35 +0100

    Refactore the setter method to one to many relationship

--- item.rb
+++ item.rb
@@ -1,39 +1,33 @@
 class Item
-  attr_accessor :genre, :author, :source, :label, :publish_date
+  attr_accessor :genre, :author, :label, :publish_date
 
-  def intitalize(_id, publish_date, archived)
+  def initialize(publish_date)
     @id = Random.rand(1..10_000)
     @publish_date = publish_date
-    @archived = archived
-    @genre = []
-    @author = []
-    @source = []
-    @label = []
+    @archived = false
   end
 
   def add_genre(genre)
-    @genre << genre
+    @genre = genre
   end
 
   def add_author(author)
-    @author << author
-  end
-
-  def add_source(source)
-    @source << source
+    @author = author
   end
 
   def add_label(label)
-    @label << label
-  end
-
-  def can_be_archived?()
-    Time.now.year - @publish_date > 10
+    @label = label
   end
 
   def move_to_archive()
     return unless can_be_archived?
 
     @archived = true
+  end
+
+  private
+
+  def can_be_archived?()
+    Time.now.year - @publish_date > 10
   end
 end
