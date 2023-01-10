72dbd6935a7451ed18c7cb25c8b8eb73ec54765e
Aminu Rabiu <arabiu033@gmail.com>
2023-01-10 15:08:39 +0100

    Fix tests

--- item.rb
+++ item.rb
@@ -1,6 +1,6 @@
 require 'date'
 class Item
-  attr_accessor :genre, :author, :label, :publish_date
+  attr_accessor :genre, :author, :source, :label, :publish_date
 
   def initialize(publish_date, archived)
     @id = Random.rand(1..10_000)
@@ -16,18 +16,22 @@ class Item
     @author = author
   end
 
+  def add_source(source)
+    @source = source
+  end
+
   def add_label(label)
     @label = label
     label.items.push(self) unless label.items.include?(self)
+  end
+
+  def can_be_archived?
+    (Time.now.year - @publish_date.year) > 10
   end
 
   def move_to_archive
     return unless can_be_archived?
 
     @archived = true
-  end
-
-  def can_be_archived?()
-    (Time.now.year - @publish_date) > 10
   end
 end
