ff5350ac59f94f4de76dee0d6be49c040307883c
Aminu Rabiu <arabiu033@gmail.com>
2023-01-10 12:08:37 +0100

    Add the class for creating albums

---
+++ album.rb
@@ -0,0 +1,15 @@
+require_relative 'item'
+
+class Album < Item
+  attr_accessor :on_spotify, :name
+
+  def initialize(name, on_spot, publish_date)
+    super(publish_date)
+    @name = name
+    @on_spotify = on_spot
+  end
+
+  def can_be_archived?
+    super().can_be_archived? && @on_spotify
+  end
+end
