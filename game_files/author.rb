1c6685b82eba91bc78d31527667b2579703c430d
nahnahAJ <nahnahahjuwah@gmail.com>
2023-01-11 15:56:52 +0000

    Convert author object data to son objects.

--- game_files/author.rb
+++ game_files/author.rb
@@ -4,7 +4,7 @@ class Author
   attr_accessor :items, :id, :first_name, :last_name
 
   # All Author class properties visible in the diagram should be defined and set up in the constructor method.
-  def initialize(_id, first_name, last_name)
+  def initialize(first_name, last_name)
     @id = Random.rand(1..100)
     @first_name = first_name
     @last_name = last_name
@@ -15,5 +15,13 @@ class Author
   def add_item(new_item)
     items << new_item
     new_item.add_author(self)
+  end
+
+  def to_json(*args)
+    {
+      'id' => @id,
+      'first_name' => @first_name,
+      'last_name' => @last_name
+    }.to_json(*args)
   end
 end
