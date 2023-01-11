14ec63ba66d8801f649b4b4051e687493fe07461
nahnahAJ <nahnahahjuwah@gmail.com>
2023-01-11 01:51:53 +0000

    Include the options_handler module in the Main class

--- main.rb
+++ main.rb
@@ -1,19 +1,23 @@
+require_relative './game_files/options_handler'
 class Main
+  def initialize
+    @games = []
+    @authors = []
+  end
+
+  include OptionsHandler
+
   def start_console
     puts 'Welcome to Catalog App'
-
     options = {
       'List all books' => :list_books,
       'List all music albums' => :list_music_albums,
-      'List all movies' => :list_movies,
       'List all games' => :list_games,
       'List all genres' => :list_genres,
       'List all labels' => :list_labels,
       'List all authors' => :list_authors,
-      'List all sources' => :list_sources,
       'Add a book' => :add_book,
       'Add a music album' => :add_music_album,
-      'Add a movie' => :add_movie,
       'Add a game' => :add_game
     }
 
@@ -30,7 +34,7 @@ class Main
       puts 'Insert any key to exit'
       input = gets.chomp.to_i
       if input.positive? && input <= 13
-        options.values[input]
+        send(options.values[input - 1])
       else
         puts 'Thank you for using this app'
         break
